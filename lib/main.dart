25f5740834c0ad5bd6cfb01fd515fef88d287912
SakunPanthi123 <spanthi863@rku.ac.in>
2023-09-23 09:44:42 +0530

    Master branch handled by Sakun

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:campusmarket/firebase_options.dart';
 import 'package:campusmarket/views/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
