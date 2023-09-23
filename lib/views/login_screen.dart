351cb202d4f177e393d0e9438207e340f7f4938b
Sabin Poudel <sabinpoudel6969@gmail.com>
2023-09-23 09:19:30 +0530

    Added a MCV pattern, did set up for Firebase Connection

---
+++ lib/views/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Login"),
+      ),
+    );
+  }
+}
