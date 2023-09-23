1af2ac375bf3a80aba783d9c7fcf8af9fba517dc
Sabin Poudel <sabinpoudel6969@gmail.com>
2023-09-23 09:47:23 +0530

    added login page, Lottie animations

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:campusmarket/views/reusable_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,11 +10,57 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Login"),
+    var size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      body: Form(
+        child: ListView(
+          children: [
+            SizedBox(
+              height: size.height * 0.45,
+              width: size.width,
+              child: Lottie.asset("asset/animations/LoginAnimation.json"),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: Column(
+                children: [
+                  customTextFormField(
+                    "Enter your email",
+                    emailController,
+                    false,
+                    const Icon(
+                      Icons.email,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  customTextFormField(
+                      "Enter your password",
+                      passwordController,
+                      true,
+                      const Icon(
+                        Icons.lock,
+                        color: Colors.black,
+                      ),
+                      const Icon(
+                        Icons.visibility,
+                        color: Colors.black,
+                      )),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
