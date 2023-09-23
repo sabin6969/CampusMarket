a4e7623b7f4ff5f733894861ac8cd67624c8c0f5
Sandesh157-Th <ssahi563@rku.ac.in>
2023-09-23 12:48:14 +0530

    hello

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -135,35 +135,140 @@ class _NewArrivalState extends State<NewArrival> {
             decoration: BoxDecoration(),
             width: MediaQuery.sizeOf(context).width,
             child: Row(children: [
+
+              SizedBox(width: 15,),
               Card(
                 elevation: 1.5,
                 color: Colors.white,
                 child: Container(
-              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(12))),
-                  width: MediaQuery.sizeOf(context).width*0.25,
-                  height: MediaQuery.sizeOf(context).height*0.2, 
-                  child: Column(
-                    
-                    children: [Container(
-                       decoration: BoxDecoration(
-                         color: Color.fromARGB(255, 232, 230, 230),
-                        borderRadius: BorderRadius.only(topLeft: Radius.circular(12),topRight:Radius.circular(12),)),
-                      width:MediaQuery.sizeOf(context).width*0.25 ,
-                     
-                      child: Icon(Icons.book_online_outlined,size: 80,color: Colors.purple,))
-                  ,
-              
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
+                  width: MediaQuery.sizeOf(context).width * 0.25,
+                  height: MediaQuery.sizeOf(context).height * 0.2,
+                  child: Column(children: [
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 232, 230, 230),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(12),
+                              topRight: Radius.circular(12),
+                            )),
+                        width: MediaQuery.sizeOf(context).width * 0.25,
+                        child: Icon(
+                          Icons.book_online_outlined,
+                          size: 80,
+                          color: Colors.purple,
+                        )),
                     SizedBox(height: 10),
-                  Text("The psychology of \nmoney")
-                  ,SizedBox(height: 10)
-                  ,Padding(
-                    padding: const EdgeInsets.only(right: 80),
-                    child: Text("Books",style: TextStyle(color: Colors.grey),),
-                  ),SizedBox(height: 10)
-                  ,Padding(
-                    padding: const EdgeInsets.only(right: 80),
-                    child: Text("Rs 500",style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),),
-                  )
+                    Text("The psychology of \nmoney"),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 80),
+                      child: Text(
+                        "Books",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 80),
+                      child: Text(
+                        "Rs 500",
+                        style: TextStyle(
+                            color: const Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                    )
+                  ]),
+                ),
+              ),
+                SizedBox(width: 25,),
+              Card(
+                elevation: 1.5,
+                color: Colors.white,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
+                  width: MediaQuery.sizeOf(context).width * 0.25,
+                  height: MediaQuery.sizeOf(context).height * 0.2,
+                  child: Column(children: [
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 232, 230, 230),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(12),
+                              topRight: Radius.circular(12),
+                            )),
+                        width: MediaQuery.sizeOf(context).width * 0.25,
+                        child: Icon(
+                          Icons.book_online_outlined,
+                          size: 80,
+                          color: Colors.purple,
+                        )),
+                    SizedBox(height: 10),
+                    Text("The psychology of \nmoney"),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 80),
+                      child: Text(
+                        "Books",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 80),
+                      child: Text(
+                        "Rs 500",
+                        style: TextStyle(
+                            color: const Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                    )
+                  ]),
+                ),
+              ),
+
+              SizedBox(width: 25,),
+              Card(
+                elevation: 1.5,
+                color: Colors.white,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
+                  width: MediaQuery.sizeOf(context).width * 0.25,
+                  height: MediaQuery.sizeOf(context).height * 0.2,
+                  child: Column(children: [
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 232, 230, 230),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(12),
+                              topRight: Radius.circular(12),
+                            )),
+                        width: MediaQuery.sizeOf(context).width * 0.25,
+                        child: Icon(
+                          Icons.book_online_outlined,
+                          size: 80,
+                          color: Colors.purple,
+                        )),
+                    SizedBox(height: 10),
+                    Text("The psychology of \nmoney"),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 80),
+                      child: Text(
+                        "Books",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 80),
+                      child: Text(
+                        "Rs 500",
+                        style: TextStyle(
+                            color: const Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                    )
                   ]),
                 ),
               ),
