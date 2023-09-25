3c93121ba71a2df245906f2173054f1721c44148
Sandesh157-Th <ssahi563@rku.ac.in>
2023-09-25 10:36:13 +0530

    homecard

--- lib/views/cards/customCard/ProductCard.dart
+++ lib/views/cards/customCard/ProductCard.dart
@@ -3,20 +3,18 @@ import 'package:flutter/material.dart';
 class CustomCardWidget extends StatelessWidget {
   final String titleText;
   final String subTitleText;
-  final Widget cardWidget;
+
   final Image cardImage;
-  final String lowerTitle;
-  final int cardWidth;
-  final IconData cardIcon;
+  final String priceTitle;
+
+  final Icon cardIcon;
 
   const CustomCardWidget({
     super.key,
     required this.titleText,
     required this.subTitleText,
     required this.cardImage,
-    required this.cardWidget,
-    required this.lowerTitle,
-    required this.cardWidth,
+    required this.priceTitle,
     required this.cardIcon,
   });
 
@@ -25,13 +23,38 @@ class CustomCardWidget extends StatelessWidget {
     return Container(
       width: MediaQuery.sizeOf(context).width * 0.25,
       height: MediaQuery.sizeOf(context).height * 0.2,
-      child: Card(child: 
-      Column(children: [
-
-        Icon(cardIcon ,size: 100,),
-        const SizedBox(height: 12,),
-        Text(titleText,    style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),)
-      ],)),
+      child: Card(
+          child: Column(
+        children: [
+          Icon(
+            cardIcon,
+            size: 100,
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Text(
+            titleText,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Text(
+            subTitleText,
+            style: const TextStyle(
+                fontSize: 15, fontWeight: FontWeight.w600, color: Colors.grey),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Text(
+            subTitleText,
+            style: const TextStyle(
+                fontSize: 15, fontWeight: FontWeight.w600, color: Colors.grey),
+          ),
+        ],
+      )),
     );
   }
 }
